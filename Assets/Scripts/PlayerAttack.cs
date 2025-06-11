c07587a8c9aa07593253c322371df8455a60c67f
AntoBlue <antonio_r@hotmail.it>
2025-06-11 22:26:49 +0200

    Projectiles implemented

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    //projectile
+    [SerializeField] GameObject Projectile;
+    [SerializeField] GameObject UpgradeProjectile;
+
+    [SerializeField] private float ProjectileSpeed;
+    [SerializeField] private float UpgradeProjectileSpeed;
+
+    [SerializeField] private Transform projectileSpawnPoint; 
+
+    private bool HasProjectile;
+    private bool HasUpgradeProjectile;
+
+
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    //if (other.gameObject.GetComponent<FireBulletsAtTarget>()) return;
+
+    //    Destroy(gameObject);
+    //}
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            var bullet = Instantiate(Projectile, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+            Projectile.GetComponent<Rigidbody>().linearVelocity = projectileSpawnPoint.up * ProjectileSpeed;
+        }
+    }
+}
