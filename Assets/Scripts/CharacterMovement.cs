6e881b22c99702c7fb2a8d10ae9e88754bd058c0
Darm94 <94damianos@gmail.com>
2025-06-13 01:29:03 +0200

    chinemachine implementation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterMovement : MonoBehaviour
 {
     [Header("Movement Settings")]
     public float moveSpeed = 5f;
+    private float airMoveSpeed = 7f;
     public float jumpForce = 7f;
     public float gravityMultiplier = 2f;
 
@@ -15,7 +17,38 @@ public class CharacterMovement : MonoBehaviour
     private Rigidbody rb;
     private bool isGrounded;
     private bool jumpPressed;
+        
+    //input system
+    [SerializeField] private InputActionAsset inputActions;
+    [SerializeField] private string actionMapName = "Player"; // o il nome che hai usato
+    [SerializeField] private string jumpActionName = "Jump";
+    private InputAction jumpAction;
+  
+    private void OnEnable()
+    {
+        var actionMap = inputActions.FindActionMap(actionMapName, true);
+        jumpAction = actionMap.FindAction(jumpActionName, true);
+        jumpAction.Enable();
+        jumpAction.performed += OnJumpPerformed;
+    }
 
+    private void OnDisable()
+    {
+        if (jumpAction != null)
+        {
+            jumpAction.performed -= OnJumpPerformed;
+            jumpAction.Disable();
+        }
+    }
+    
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            jumpPressed = true;
+        }
+    }
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -25,44 +58,58 @@ public class CharacterMovement : MonoBehaviour
     private void Update()
     {
         // Input salto
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            jumpPressed = true;
-        }
+        //if (Input.GetButtonDown("Jump") && isGrounded)
+        //{
+            //jumpPressed = true;
+        //}
 
         // Ground check
         isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
         
         // Flip personaggio
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        if (horizontalInput != 0)
+        /*if (horizontalInput != 0)
         {
             Vector3 scale = transform.localScale;
             scale.x = Mathf.Sign(horizontalInput) * Mathf.Abs(scale.x);
             transform.localScale = scale;
+        }*/
+        if (horizontalInput != 0)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0, horizontalInput > 0 ? 0 : 180, 0);
+            transform.rotation = targetRotation;
         }
     }
 
     private void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+        float currentSpeed = isGrounded ? moveSpeed : airMoveSpeed;
 
         // Movimento orizzontale
-        Vector3 velocity = rb.linearVelocity;
+       /* Vector3 velocity = rb.linearVelocity;
         velocity.x = horizontalInput * moveSpeed;
+        rb.linearVelocity = velocity;*/
+        Vector3 velocity = rb.linearVelocity;
+        velocity.x = horizontalInput * currentSpeed;
         rb.linearVelocity = velocity;
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        Debug.Log($"isGrounded: {isGrounded}, jumpPressed: {jumpPressed}");
 
         // Salto
         if (jumpPressed)
         {
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+            //rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+
             jumpPressed = false;
         }
 
         // Gravità 
-        if (!isGrounded && rb.linearVelocity.y < 0)
+        if (!isGrounded && rb.velocity.y < 0)
         {
-            rb.linearVelocity += Vector3.up * Physics.gravity.y * (gravityMultiplier - 1) * Time.fixedDeltaTime;
+            rb.velocity += Vector3.up * Physics.gravity.y * (gravityMultiplier - 1) * Time.fixedDeltaTime;
         }
     }
 
