17a5a41b962db54934bee9f73224f02fdf7bb1c3
Darm94 <94damianos@gmail.com>
2025-06-12 22:21:21 +0200

    fix: sceneSelector

--- Assets/Scripts/MenuScripts/BackToSelector.cs
+++ Assets/Scripts/MenuScripts/BackToSelector.cs
@@ -1,16 +1,47 @@
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class BackToSelector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private InputActionAsset inputActions;
+    [SerializeField] private string actionMapName = "Player"; // o il nome corretto del tuo action map
+    [SerializeField] private string actionName = "BackInspector"; // il nome esatto della tua InputAction
+    [SerializeField] private SceneAsset backScene;
+    private string sceneToLoad;
+
+    private InputAction backAction;
+
+    private void Start()
     {
-        
+        sceneToLoad = backScene.name;
+    }
+ 
+    private void OnEnable()
+    {
+        var actionMap = inputActions.FindActionMap(actionMapName, true);
+        backAction = actionMap.FindAction(actionName, true);
+        backAction.Enable();
+        backAction.performed += OnBackPressed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        backAction.performed -= OnBackPressed;
+        backAction.Disable();
+    }
+
+    public void OnBackPressed(InputAction.CallbackContext context)
+    {
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("Nessuna scena specificata!");
+        }
     }
 }
