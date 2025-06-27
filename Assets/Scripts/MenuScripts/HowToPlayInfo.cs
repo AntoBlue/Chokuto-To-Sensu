c9cdb47835c2005678230bc75295712c9fed0f85
rocco <roxofficial3@gmail.com>
2025-06-27 12:32:45 +0200

    How to play canvas

---
+++ Assets/Scripts/MenuScripts/HowToPlayInfo.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class HowToPlayInfo : MonoBehaviour
+{
+    [SerializeField] private GameObject canvas;
+    void Start()
+    {
+        canvas.gameObject.SetActive(false);
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        canvas.gameObject.SetActive(true);
+    }
+    
+
+
+    public void OnTriggerExit(Collider other)
+    {
+        canvas.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
